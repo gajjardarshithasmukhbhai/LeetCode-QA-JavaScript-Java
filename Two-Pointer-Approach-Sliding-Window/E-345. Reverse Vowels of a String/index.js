cd830fb0a6e34a08577f2467b5ccb326ebbaf86b
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-09-28 01:14:24 +0530

    add new

--- Two-Pointer-Approach-Sliding-Window/E-345. Reverse Vowels of a String/index.js
+++ Two-Pointer-Approach-Sliding-Window/E-345. Reverse Vowels of a String/index.js
@@ -28,3 +28,30 @@ const reverseVowels = (s) =>  {
      }
      return splitValue.join('')
 };
+
+
+// more optimized
+
+const reverseVowels = (s) =>  {
+     const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+     
+     let splitValue = s.split('');
+
+     let left = 0;
+     let right = splitValue.length-1;
+
+     while(left<=right) {
+        if(!vowels.includes(splitValue[left])) {
+            left++;
+            continue;
+        }
+        else if(!vowels.includes(splitValue[right])) {
+            right--;
+            continue;
+        }
+        [splitValue[left], splitValue[right]] = [splitValue[right], splitValue[left]]
+        right--;
+        left++;
+    }
+     return splitValue.join('')
+};
