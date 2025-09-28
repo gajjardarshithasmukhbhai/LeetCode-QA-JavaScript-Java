3ef2aeb5786b008ff6068e5afede77b0d5eebfa5
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-09-28 19:34:30 +0530

    add problem

--- debug-file.js
+++ debug-file.js
@@ -12,7 +12,7 @@ const maxSubarrayLength = (nums, k) => {
         
         frequencyCount.set(nums[right], (frequencyCount.get(nums[right]) || 0)+1);
 
-        while(frequencyCount.get(nums[left]) > k) {
+        while(frequencyCount.get(nums[right]) > k) {
             
             frequencyCount.set(nums[left], frequencyCount.get(nums[left])-1);
 
