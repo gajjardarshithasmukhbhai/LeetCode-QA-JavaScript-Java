04f8fc5bfe651ce051c732998ae1060900d3fba7
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-09-30 00:17:35 +0530

    add new problem

--- Two-Pointer-Approach-Sliding-Window/E-3258. Count Substrings That Satisfy K-Constraint I/Solution.java
+++ Two-Pointer-Approach-Sliding-Window/E-3258. Count Substrings That Satisfy K-Constraint I/Solution.java
@@ -1,3 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    
+    public int countKConstraintSubstrings(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while (map.getOrDefault('0', 0) > k && map.getOrDefault('1', 0) > k) {
+                if (map.getOrDefault('0', 0) > k || map.getOrDefault('1', 0) > k) {
+                    char leftChar = s.charAt(left);
+                    map.put(leftChar, map.get(leftChar) - 1);
+                    left++;
+                }
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countKConstraintSubstrings("10101", 1));
+    }
 }
