1f36adb1993978d265221adc6db9029daeb34989
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-10-29 08:24:15 +0530

    added

--- debug-file.js
+++ debug-file.js
@@ -1,34 +1,18 @@
-// brutforce solution
-const countValidSelections = (nums) => {
-    let result = 0;
+var removeDuplicates = function(nums) {
+    let left = 0;
+    let right = 1;
 
-    for(let i=0;i<nums.length;i++) {
-        if(nums[i]>0) continue;
+    while(right < nums.length) {
 
-        // this is for left and right direction
-        for(let direction=0; direction<2; direction++) {
-            let shadowNums = nums.slice();
-            let index = i;
-            let directionLeftOrRight = direction === 0 ? -1: 1;
-
-            while (index >= 0 && index < shadowNums.length) {
-                if(shadowNums[index] === 0) {
-                    index +=directionLeftOrRight;
-                }
-                else if(shadowNums[index] > 0) {
-                    shadowNums[index] = shadowNums[index]-1;
-                    directionLeftOrRight *= -1;
-                    index +=directionLeftOrRight;
-                }
-                else {
-                    break;
-                }
-            }
-            if(shadowNums.every(nums => nums === 0)) {
-                result++;
-            }
+        if(nums[left]!=nums[right]) {
+            left++;
+            nums[left] = nums[right];
+        }
+        else {
+            right++;
         }
     }
-    return result;
+    return left;
 };
-console.log(countValidSelections([1,0,2,0,3]));
+
+console.log(removeDuplicates([1,1,2]));
