16e99137419eb7c452a9aab640cd3e55d23e2bce
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2025-11-02 21:43:16 +0530

    add new problem

--- debug-file.js
+++ debug-file.js
@@ -1,23 +1,19 @@
 const zeroFilledSubarray = (nums) => {
 
-    let stack = [];
-    
     let count = 0;
     let result = 0;
 
     for(let i=0;i<nums.length;i++) {
         if(nums[i] === 0) {
-            stack.push(0);
+            count++;
         }
-        if(stack.length && stack[stack.length-1] === nums[i]) {
-            ++count;
-        }
-        if(stack.length && stack[stack.length-1]!==nums[i]) {
+        else {
             result += ((count)*(count+1))/2;
             count = 0;
-            stack = [];
         }
     }
+
+    // if it ends with zero
     return result;
 }
 console.log(zeroFilledSubarray([0,0,0,2,0,0]));
